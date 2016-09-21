ea00750ecf5ef51a4c4db1ceed2d2f30b4088873
Daniel Herzig <dxh9845@rit.edu>
2016-09-21 19:45:38 -0400

    Added input filling to the form.w

---
+++ Trump.js
@@ -0,0 +1,21 @@
+(function(win, doc, $, undefined) {
+    'use strict';
+    
+    // Don't run if jQuery isn't loaded
+    if (typeof window.jQuery === 'undefined') {
+      return;
+    }
+    
+    // Our code will go here.
+
+    $('input[type=text]')
+    	.val('Vladmir Putin paid me to do this.')
+    	.prev().find('input').prop('checked', true);
+    $('textarea')
+    .val('Boy oh boy here I go fillin these forms!! \
+    	I love to fill these forms for my candidate! \
+    	Really makes me feel good about their convictions, \
+    	 you know, when they ask their constitutents what they should think!! \
+    	 Oh boy.  ');
+
+  }(window, window.document, window.jQuery));
