e25449f8423addc04511171a932ea40361cdd8a9
Daniel Herzig <dxh9845@rit.edu>
2016-09-22 20:41:33 -0400

    Modified Trump.js to pass form validation

--- Trump.js
+++ Trump.js
@@ -9,7 +9,7 @@
     // Our code will go here.
 
     $('input[type=text]')
-    	.val('Vladmir Putin paid me to do this.')
+    	.val('Putin paid to do this');
     	.prev().find('input').prop('checked', true);
     $('textarea')
     .val('Boy oh boy here I go fillin these forms!! \
@@ -18,8 +18,10 @@
     	 you know, when they ask their constitutents what they should think!! \
     	 Oh boy.  ');
 
+
+    $('#id_question_2866_2').prop('checked');
     $('input#id_full_name').val("Vladdy P");
-    $('input#id_email').val('killmepls@gmail.com');
+    $('input#id_email').val('killmepls@gmail.com')
     $('input#id_postal_code').val('48169');
 
   }(window, window.document, window.jQuery));
