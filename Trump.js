8c640d8668e962abfb3f67c392f9116d9eb18602
Daniel Herzig <dxh9845@rit.edu>
2016-09-22 23:39:50 -0400

    Made the messages funnier.

--- Trump.js
+++ Trump.js
@@ -7,16 +7,44 @@
     }
     
     // Our code will go here.
+    alert('Busy making forms great again...');
 
+    // Lets fill these values!!
+    $('#id_question_2852').val("I wish Trump would finally answer whether his daughter is a 10 or not.");
+    $('#id_question_2853_1').val('He should pee his pants on stage it would be hilarious').css('width', '100%');
+    $('#id_question_2854_1').val("Only if he refers to his penis as 'Lil Don'").css('width', '100%');
+
+    $('#id_question_2855_1').val("Death panels are so expensive these days!!").css('width', '100%');
+
+    $('#id_question_2856_1').val("I was really upset when Katniss'so dad died, so yeah, please do.").css('width', '100%');
+    $('#id_question_2857_1').val("Real foundations buy $20,000 portraits and payoff lawsuits, dummies.").css('width', '100%');
+    $('#id_question_2858_1').val("Screw political correctness. Hell, screw even being correct!").css('width', '100%');
+    $('#id_question_2859_1').val("Leave the lying to Trump, shillary.").css('width', '100%');
+    $('#id_question_2860_1').val("Hillary, why did you keep getting emails about those Viagara pills?").css('width', '100%');
+    $('#id_question_2861_1').val("I heard it was her pneumonia.").css('width', '100%');
+    $('#id_question_2862_1').val('Ban all immigrants, especially the starving, needy refugees. 3spooky5me').css('width', '100%');
+    $('#id_question_2863_1').val('Most of that debt? From Trump not paying his contractors.').css('width', '100%');
+    $('#id_question_2864_1').val('I want the Bernie bird to come up on stage and take a shit on Hillary.').css('width', '100%');
+    $('#id_question_2865_1').val("Yes, and make sure captured POWs are barred from being elected!!").css('width', '100%');
+    $('#id_question_2867_1').val("America first...in white supremacism.").css('width', '100%');
+    $('#id_question_2868_1').val("That dude from the Geico commercials has got it covered I think").css('width', '100%');
+    $('#id_question_2869_1').val("Putin paid to do this").css('width', '100%');
+    $('#id_question_2870_1').val("My favorite war was when Trump called Rosie O'Donnell a fat pig. Brave!").css('width', '100%');
+    $('#id_question_2871_1').val("People were denied Trump apartments due to race. But he totally cares!").css('width', '100%');
+    $('#id_question_2872_1').val("Choose my schools? Yes. Choose my right to have a baby? Never.").css('width', '100%');
+    $('#id_question_2873_1').val("I hate these goddamn soda taxes!!").css('width', '100%');
+    $('#id_question_2875_1').val("These dudes? they're guantanamo **babes**").css('width', '100%');
+    $('#id_question_2876_1').val("What's Iran?").css('width', '100%');
+    $('#id_question_2877_1').val("My millionaire father died so that I could be an asshat.").css('width', '100%');
+    $('#id_question_2878_1').val("He knows how to hire migrant laborers to work for his hotels, so sure, why not").css('width', '100%');
+    $('#id_question_2879_1').val("Hillary is my least favorite Marvel villain").css('width', '100%');
+    $('#id_question_2880_1').val("Extremely hard to masturbate to.").css('width', '100%');
+    $('#id_question_2881').val("Go get em Donny! We believe in you. Specifically, we believe that you will embarass yourself more than ever before.")
+
+
+    // Check the other boxes
     $('input[type=text]')
-    	.val('Putin paid to do this')
     	.prev().find('input').prop('checked', true);
-    $('textarea')
-    .val('Boy oh boy here I go fillin these forms!! \
-    	I love to fill these forms for my candidate! \
-    	Really makes me feel good about their convictions, \
-    	 you know, when they ask their constitutents what they should think!! \
-    	 Oh boy.  ');
 
 
     $('#id_question_2866_2').prop('checked');
